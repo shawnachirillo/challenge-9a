d80e82d81095569d13b96d9b19bec06e285fa75b
Shawna Chirillo <schirillo711@gmail.com>
2025-03-01 19:19:18 -0600

    modified:   server/src/routes/htmlRoutes.ts

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -6,5 +6,9 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
+router.get('/', (_req, res) => {
+  const filePath = path.join(__dirname, '../../../client/dist/index.html');
+  res.sendFile(filePath);
+});
 
 export default router;
