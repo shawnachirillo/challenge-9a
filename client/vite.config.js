34b58bd73d0610760236a64e7a639dc177003498
Shawna Chirillo <schirillo711@gmail.com>
2025-04-13 00:30:03 -0500

    Add frontend env config and fix API base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        // target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  preview: {
-    port: 4173,
-    allowedHosts: ['challenge-9a-frontend.onrender.com'],
-  },
+    preview: {
+      port: 4173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
